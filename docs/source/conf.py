3589e70298880e144f1ff7ad60dbf97f6275962d
Andreas Klose <aklose@math.au.dk>
2026-09-12 15:28:54 +0200

    Changed Sphinx conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import os, sys
-sys.path.insert(0,os.path.abspath('/home/andreas/p/python/PyLoa/'))
+sys.path.insert(0,os.path.abspath('../../'))
 
 project = 'PyLoa'
 copyright = '2026, Andreas Klose'
